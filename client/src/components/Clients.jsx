e485d5b05e3af7d80d03575f715d08aff1c436b6
Avani Sindhav <avani@simformsolutions.com>
2023-03-28 11:28:40 +0530

    client fetcg query 28-march-2023

--- client/src/components/Clients.jsx
+++ client/src/components/Clients.jsx
@@ -1,24 +1,37 @@
 import { gql, useQuery } from "@apollo/client";
-
-const GET_CLIENT = gql`
-  query getClients {
-    clients {
-      id
-      name
-      email
-    }
-  }
-`;
+import ClientRow from "./ClientRow";
+import { GET_CLIENT } from "../queries/ClientQueries";
+import Spinner from "./Spinner";
 
 export default function Clients() {
   const { loading, error, data } = useQuery(GET_CLIENT);
 
   if (loading) {
-    return <p>loading...</p>;
+    return <Spinner />;
   }
 
   if (error) {
     return <p>something went wrong..</p>;
   }
-  return <>{!loading && !error && <h1>clients</h1>}</>;
+  return (
+    <>
+      {!loading && !error && (
+        <table className="table mt-3 table-hover">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Phone</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.clients.map((client) => {
+              return <ClientRow key={client.id} client={client} />;
+            })}
+          </tbody>
+        </table>
+      )}
+    </>
+  );
 }
---
+++ client/src/queries/ClientQueries.js
@@ -0,0 +1,11 @@
+import { gql } from "@apollo/client";
+export const GET_CLIENT = gql`
+  query getClients {
+    clients {
+      id
+      name
+      email
+      phone
+    }
+  }
+`;
